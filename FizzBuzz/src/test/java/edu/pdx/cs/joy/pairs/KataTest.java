ce72dd7f49d00d368c51291da63845bfa87e0eca
nmetens <metens@pdx.edu>
2025-01-15 20:17:50 -0800

    Refactor

--- FizzBuzz/src/test/java/edu/pdx/cs/joy/pairs/KataTest.java
+++ FizzBuzz/src/test/java/edu/pdx/cs/joy/pairs/KataTest.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs.joy.pairs;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,26 +9,36 @@ import static org.hamcrest.Matchers.equalTo;
 public class KataTest
 {
 
+  private Kata kata;
+
+  @BeforeEach
+  void setUp() {
+    kata = new Kata();
+  }
+  
+
   @Test
   void canInstantiateKataClass() {
     new Kata();
   }
 
+
   @Test
   void fiveBuzzes() {
-    Kata kata = new Kata();
-    assertThat(kata.fizzBuzz(5), equalTo("Buzz") );
+      assertFizzBuzz(5, "Buzz");
+  }
+
+  private void assertFizzBuzz(int buzzNumber, String buzzString) {
+    assertThat(kata.fizzBuzz(buzzNumber), equalTo(buzzString) );
   }
 
   @Test
   void threeFizzes() {
-    Kata kata = new Kata();
     assertThat(kata.fizzBuzz(3), equalTo("Fizz") );
   }
 
   @Test
   void twentyThreeIs23() {
-    Kata kata = new Kata();
     assertThat(kata.fizzBuzz(23), equalTo("23") );
   }
 }
