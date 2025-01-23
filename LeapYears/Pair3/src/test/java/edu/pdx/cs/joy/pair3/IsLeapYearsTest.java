1bb76fac7fe545e3ddfa4ce3f4bfffb4c0f7a464
albadri <albadri@pdx.edu>
2025-01-22 19:56:02 -0800

    Pair 3

---
+++ LeapYears/Pair3/src/test/java/edu/pdx/cs/joy/pair3/IsLeapYearsTest.java
@@ -0,0 +1,30 @@
+package edu.pdx.cs.joy.pair3;
+
+import org.junit.jupiter.api.Test;
+
+public class IsLeapYearsTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new IsLeapYears();
+  }
+
+  @Test
+  void isNotLeapYear(){
+    IsLeapYears check = new IsLeapYears();
+    assert(false == check.isLeapYear(2001));
+  }
+
+  @Test
+  void isLeapYear(){
+    IsLeapYears check = new IsLeapYears();
+    assert(true == check.isLeapYear(2400));
+  }
+
+  @Test
+  void isNotLeapfor100s(){
+    IsLeapYears check = new IsLeapYears();
+    assert(false == check.isLeapYear(2100));
+  }
+}
