34f2a3263e5dacd1d0faa7b651c6add024b74896
tejaswi <battula@pdx.edu>
2025-01-22 19:55:41 -0800

    functions to check divisibility

--- LeapYears/Pair9/src/main/java/edu/pdx/cs/joy/pair9/LeapYears.java
+++ LeapYears/Pair9/src/main/java/edu/pdx/cs/joy/pair9/LeapYears.java
@@ -10,8 +10,25 @@ import com.google.common.annotations.VisibleForTesting;
  */
 public class LeapYears {
 
+  int year;
   @VisibleForTesting
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
+  }
+
+  public LeapYears(int year){
+    this.year=year;
+  }
+
+  public boolean yearDivisibleByFour(){
+    return year%4==0;
+  }
+
+  public boolean yearDivisibleByFourHundred(){
+    return year%400==0;
+  }
+
+  public boolean yearDivisibleByHundred(){
+    return year%100==0;
   }
 }
