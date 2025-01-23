920fa84c93b446a577b5263b1567f7f815f92d6c
Travis Tran <trantrav@pdx.edu>
2025-01-22 20:22:53 -0800

    Refactor leapYear for testing

--- LeapYears/Pair12/src/test/java/edu/pdx/cs/joy/pair12/LeapYearsTest.java
+++ LeapYears/Pair12/src/test/java/edu/pdx/cs/joy/pair12/LeapYearsTest.java
@@ -1,5 +1,6 @@
 package edu.pdx.cs.joy.pair12;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,33 +9,33 @@ import static org.hamcrest.Matchers.equalTo;
 public class LeapYearsTest
 {
 
+  private LeapYears leapYears;
+
   @Test
   void canInstantiateKataClass() {
     new LeapYears();
   }
 
+  @BeforeEach
+  void setup() { leapYears = new LeapYears();}
   @Test
   void check2000()
   {
-    LeapYears leapYears = new LeapYears();
     assertThat(leapYears.isLeapYear(2000), equalTo(true));
   }
 
   @Test
   void check1700() {
-    LeapYears leapYears = new LeapYears();
     assertThat(leapYears.isLeapYear(1700), equalTo(false));
   }
 
   @Test
   void check2008() {
-    LeapYears leapYears = new LeapYears();
     assertThat(leapYears.isLeapYear(2008), equalTo(true));
   }
 
   @Test
   void check2017() {
-    LeapYears leapYears = new LeapYears();
     assertThat(leapYears.isLeapYear(2017), equalTo(false));
   }
 }
