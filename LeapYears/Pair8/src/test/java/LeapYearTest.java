3857531089df7e8b01911335a40bdf014d03f858
Tlabting <teresalabolle@gmail.com>
2025-01-22 20:12:41 -0800

    Pair 8 round 3

--- LeapYears/Pair8/src/test/java/LeapYearTest.java
+++ LeapYears/Pair8/src/test/java/LeapYearTest.java
@@ -1,19 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+import static edu.pdx.cs.joy.pair8.LeapYears.leapYears;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LeapYearTest {
-    void yearsDivisibleBy400Are(int year) {
-        assertTrue(year % 400 == 0);
+    @Test
+    void yearsDivisibleBy400Are() {
+        int year = 2000;
+        assertTrue(leapYears(year));
     }
 
-    void yearsDivisibleBy100ButNot400Not(int year) {
-        assertFalse(year % 100 == 0 && year % 400 != 0);
+    @Test
+    void yearsDivisibleBy100ButNot400Not() {
+        assertFalse(leapYears(1700));
+        assertFalse(leapYears(1800));
+        assertFalse(leapYears(1900));
+        assertFalse(leapYears(2100));
     }
 
+    @Test
     void yearsDivisibleBy4AndNot100Are(int year) {
         assertTrue(year % 4 == 0 && year % 100 != 0);
     }
 
+    @Test
     void yearsDivisibleBy4AreNot(int year) {
         assertFalse(year % 4 == 0);
     }
