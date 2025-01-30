a4b8bf143f11ba61d668afd67c4ff90f3ebdb397
satvikmudgal <satvik.mudgal@gmail.com>
2025-01-29 20:15:08 -0800

    Implemented all functions for printing Diamond

--- Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
+++ Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
@@ -2,6 +2,10 @@ package edu.pdx.cs.joy.pair4;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * A class for getting started with a code kata
  *
@@ -12,6 +16,13 @@ public class Diamond {
 
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+    char letter = args[0].charAt(0);
+    int index = alpha.indexOf(String.valueOf(letter));
+    Diamond.printTopRow(index);
+    if (index != 0){
+      // print remaining rows
+      printRows(index - 1, 1, index, true);
+    }
+    printTopRow(0);
   }
 }
