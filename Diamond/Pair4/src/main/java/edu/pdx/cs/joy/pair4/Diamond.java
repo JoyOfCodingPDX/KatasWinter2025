5b64ab106224bd4843e89c1b80ab3bb935497ced
satvikmudgal <satvik.mudgal@gmail.com>
2025-01-29 20:16:34 -0800

    Implemented Diamond

--- Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
+++ Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
@@ -13,6 +13,48 @@ import java.util.Scanner;
  * class (and its tests).
  */
 public class Diamond {
+   static ArrayList<String> alpha = new ArrayList<String>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"));
+
+  public void print(String letter, int index){
+    System.out.print(letter);
+    if (index > 0) {
+      printSpaces(index);
+    }
+  }
+
+  private static void printSpaces(int index) {
+    for (int i = 0; i < index; i++) {
+      System.out.print(" ");
+    }
+  }
+
+  private static void printTopRow(int index) {
+    printSpaces(index);
+    System.out.print(alpha);
+  }
+
+  private static int getSpacesInMiddle(int curr, boolean building) {
+    return building ? curr + 2 : curr - 2;
+  }
+  private static int getSpacesBeforeAfter(int curr, boolean building) {
+    return building ? curr - 1 : curr + 1;
+  }
+
+
+  private static void printRows(int spacesBefore, int spacesInMiddle, int index, boolean building) {
+    if(spacesBefore == index - 1 && !building) {
+      return;
+    }
+    printSpaces(spacesBefore);
+    System.out.print(alpha);
+    printSpaces(spacesInMiddle);
+    System.out.println();
+    if (spacesBefore <= 0) {
+      building = !building;
+    }
+    printRows(getSpacesBeforeAfter(spacesBefore, building), getSpacesInMiddle(spacesInMiddle, building), index, building);
+  }
+
 
   @VisibleForTesting
   public static void main(String[] args) {
