2d845d1a31230c3cb211c25d18e57e99016aae49
tejaswi <battula@pdx.edu>
2025-01-29 19:45:29 -0800

    diamond logic

--- Diamond/Pair9/src/test/java/edu/pdx/cs/joy/pair9/DiamondTest.java
+++ Diamond/Pair9/src/test/java/edu/pdx/cs/joy/pair9/DiamondTest.java
@@ -1,11 +1,11 @@
 package edu.pdx.cs.joy.pair9;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
