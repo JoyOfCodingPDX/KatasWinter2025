f35cbb387bf7fd51f94c37cf7d90c8b57cc1bc63
Brendan Chang <brendanc@pdx.edu>
2025-01-29 19:40:59 -0800

    pushhh

--- Diamond/Pair14/src/main/java/edu/pdx/cs/joy/pair14/Diamond.java
+++ Diamond/Pair14/src/main/java/edu/pdx/cs/joy/pair14/Diamond.java
@@ -19,22 +19,18 @@ public class Diamond {
   public void drawLine(char b) {
     //System.out.println(position);
     //System.out.println(Character.toLowerCase(b));
+    int position = Character.toUpperCase(b) - 'A' + 1;
+    int betweeenSpaces = 1;
     for (int j = 65; j <= 90; j++) {
 
-      int position = Character.toUpperCase(j) - 'A' + 1;
       int leadingSpaces = position - 1;
-      int preLeadingSpaces = Character.toUpperCase(b) - 'A' + 1;
-
-//      if (position > 1) {
-//        System.out.print((char)j);
-//      }
 
       for (int i = 0; i < leadingSpaces; i++) {
         System.out.print(" ");
       }
 
       System.out.println((char)j);
-
+      position--;
       if (j == (int)b) {
         break;
       }
