3fbf8ae463fffbd1c08aeac353ffa24dc17d7e98
Taylor Pettingill <134963416+Kungen13@users.noreply.github.com>
2025-01-29 19:52:49 -0800

    Taylor print middle

--- Diamond/Pair16/src/main/java/edu/pdx/cs/joy/pair16/Diamond.java
+++ Diamond/Pair16/src/main/java/edu/pdx/cs/joy/pair16/Diamond.java
@@ -29,11 +29,22 @@ public class Diamond {
   {
     String pattern = "";
     int index = findIndex(theLetter);
+    int space = index;
 
     //String result = String.valueOf(ch).repeat(n);
 
-    for (int i = 0; i < index; i++) {
-      System.out.print(theLetter);
+    for (int i = 0; i < index + 1; i++) {
+      String line = "";
+      line = String.valueOf(" ").repeat(space);
+      if(i == 0)
+      {
+        line += alphabet.charAt(i);
+      }
+      else
+      {
+        line += alphabet.charAt(i) + String.valueOf(" ").repeat(i * 2 -1) + alphabet.charAt(i);
+      }
+      --space;
     }
     return pattern;
   }
