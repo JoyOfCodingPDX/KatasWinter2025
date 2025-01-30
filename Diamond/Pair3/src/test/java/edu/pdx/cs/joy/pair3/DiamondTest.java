e8d7e986d7063c5ff8f67a794b643cab36f0effc
Hemu <hemu@pdx.edu>
2025-01-29 20:17:50 -0800

    Implemented diamond

--- Diamond/Pair3/src/test/java/edu/pdx/cs/joy/pair3/DiamondTest.java
+++ Diamond/Pair3/src/test/java/edu/pdx/cs/joy/pair3/DiamondTest.java
@@ -1,13 +1,111 @@
 package edu.pdx.cs.joy.pair3;
 
 import org.junit.jupiter.api.Test;
+<<<<<<< HEAD
 
 public class DiamondTest
 {
+=======
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DiamondTest {
+
+    @Test
+    void canInstantiateDiamondClass() {
+        new Diamond();
+    }
+
+    private String captureOutput(char inputChar) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        Diamond.printDiamond(inputChar);
+
+        System.setOut(originalOut);
+        return outputStream.toString().trim();
+    }
+>>>>>>> 0c844f3 (Implemented diamond)
 
   @Test
   void canInstantiateKataClass() {
     new Diamond();
   }
 
+<<<<<<< HEAD
+=======
+    @Test
+    void testDiamondB() {
+        String expected =
+                " A\n" +
+                "B B\n" +
+                " A";
+        assertEquals(expected, captureOutput('B'));
+    }
+
+    @Test
+    void testDiamondC() {
+        String expected =
+                "  A\n" +
+                " B B\n" +
+                "C   C\n" +
+                " B B\n" +
+                "  A";
+        assertEquals(expected, captureOutput('C'));
+    }
+
+    @Test
+    void testDiamondD() {
+        String expected =
+                "   A\n" +
+                "  B B\n" +
+                " C   C\n" +
+                "D     D\n" +
+                " C   C\n" +
+                "  B B\n" +
+                "   A";
+        assertEquals(expected, captureOutput('D'));
+    }
+
+    @Test
+    void testInvalidCharacter() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalErr = System.err;
+        System.setErr(new PrintStream(outputStream));
+
+        String[] args = {"1"};
+        Diamond.main(args);
+
+        System.setErr(originalErr);
+        assertTrue(outputStream.toString().contains("Invalid input: Please enter a letter from A to Z"));
+    }
+
+    @Test
+    void testMissingArgument() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalErr = System.err;
+        System.setErr(new PrintStream(outputStream));
+
+        String[] args = {};
+        Diamond.main(args);
+
+        System.setErr(originalErr);
+        assertTrue(outputStream.toString().contains("Missing command line arguments"));
+    }
+
+    @Test
+    void testTooManyArguments() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalErr = System.err;
+        System.setErr(new PrintStream(outputStream));
+
+        String[] args = {"A", "B"};
+        Diamond.main(args);
+
+        System.setErr(originalErr);
+        assertTrue(outputStream.toString().contains("Too many arguments. Please provide a single letter."));
+    }
+>>>>>>> 0c844f3 (Implemented diamond)
 }
