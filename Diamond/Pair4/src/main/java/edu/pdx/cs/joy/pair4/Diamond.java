7c62d5cfd1d1e2f8b5f39333782e9e847a31adff
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-01-31 21:09:43 -0800

    Finished Diamond Kata

--- Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
+++ Diamond/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Diamond.java
@@ -15,26 +15,26 @@ public class Diamond {
   static ArrayList<String> alpha = new ArrayList<String>(Arrays.asList("A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
       "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"));
 
-  private static void printRows(int spacesInMiddle, int curr, int index) {
+  private static void printSingleCharRow(int index) {
+    System.out.println(" ".repeat(index) + alpha.get(0));
+  }
+
+  private static void printDoubleCharRow(int spacesInMiddle, int curr, int index) {
     System.out.println(" ".repeat(index - curr) + alpha.get(curr) + " ".repeat(spacesInMiddle) + alpha.get(curr));
     if (curr == index || index == 0) {
       return;
     }
-    printRows(spacesInMiddle + 2, curr + 1,
-        index);
+    printDoubleCharRow(spacesInMiddle + 2, curr + 1, index);
     System.out.println(" ".repeat(index - curr) + alpha.get(curr) + " ".repeat(spacesInMiddle) + alpha.get(curr));
   }
 
   @VisibleForTesting
   public static void main(String[] args) {
-    char letter = args[0].charAt(0);
-    int index = alpha.indexOf(String.valueOf(letter));
-    int curr = 0;
-    System.out.println(" ".repeat(index) + alpha.get(0));
+    int index = alpha.indexOf(String.valueOf(args[0].charAt(0)));
+    printSingleCharRow(index);
     if (index != 0) {
-      // print remaining rows
-      printRows(1, curr + 1, index);
-      System.out.println(" ".repeat(index) + alpha.get(0));
+      printDoubleCharRow(1, 1, index);
+      printSingleCharRow(index);
     }
   }
 }
