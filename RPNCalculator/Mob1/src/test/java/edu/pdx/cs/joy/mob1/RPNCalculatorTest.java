262e905548b8ea5ebfa65a465bb410180244816e
David Whitlock <david.m.whitlock@gmail.com>
2025-02-05 20:23:48 -0800

    Rename Kata to RPNCalculator.

--- RPNCalculator/Mob1/src/test/java/edu/pdx/cs/joy/mob1/RPNCalculatorTest.java
+++ RPNCalculator/Mob1/src/test/java/edu/pdx/cs/joy/mob1/RPNCalculatorTest.java
@@ -2,12 +2,12 @@ package edu.pdx.cs.joy.mob1;
 
 import org.junit.jupiter.api.Test;
 
-public class KataTest
+public class RPNCalculatorTest
 {
 
   @Test
   void canInstantiateKataClass() {
-    new Kata();
+    new RPNCalculator();
   }
 
 }
