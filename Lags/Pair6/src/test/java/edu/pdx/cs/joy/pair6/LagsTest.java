208e1e0e61fa0fcf8a4f25b7a5aff302ff4e2cd6
tahniat <tahniat@pdx.edu>
2025-02-05 19:59:30 -0800

    added a test compare

--- Lags/Pair6/src/test/java/edu/pdx/cs/joy/pair6/LagsTest.java
+++ Lags/Pair6/src/test/java/edu/pdx/cs/joy/pair6/LagsTest.java
@@ -24,4 +24,12 @@ public class LagsTest
     lagsList.add(new Lags(1, 2, 3));
   }
 
+  @Test
+  void canCompareLags(){
+
+    Lags lag1 = new Lags(1, 2, 3);
+    Lags lag2 = new Lags(3, 5, 2);
+    
+  }
+
 }
