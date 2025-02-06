404aeeedb0c84ebcbf158e93b252a132d0d51815
Brendan Chang <brendanc@pdx.edu>
2025-02-05 19:47:31 -0800

    group 6 update

--- Lags/Pair6/src/test/java/edu/pdx/cs/joy/pair6/LagsTest.java
+++ Lags/Pair6/src/test/java/edu/pdx/cs/joy/pair6/LagsTest.java
@@ -2,12 +2,26 @@ package edu.pdx.cs.joy.pair6;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LagsTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new Lags();
+  }
+
+  @Test
+  void canInstantiateNonEmptyKataClass() {
+    new Lags(1, 2, 3);
+  }
+
+  @Test
+  void canMakeArrayOfLags() {
+    List<Lags> lagsList = new ArrayList<>();
+    lagsList.add(new Lags(1, 2, 3));
   }
 
 }
