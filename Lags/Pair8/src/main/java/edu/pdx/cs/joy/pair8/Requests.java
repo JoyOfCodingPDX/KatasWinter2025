7fc03c74ea3debdf0396d2a47c4f247b904bcb8c
GoodGuyGregory <greg.witt625@gmail.com>
2025-02-05 19:46:29 -0800

    adds canCreateRequest and Request

---
+++ Lags/Pair8/src/main/java/edu/pdx/cs/joy/pair8/Requests.java
@@ -0,0 +1,21 @@
+package edu.pdx.cs.joy.pair8;
+
+public class Requests {
+
+    // attributes
+    String plane;
+    Integer startTime;
+    Integer endTime;
+    Integer price;
+    Integer duration;
+
+    public Requests(String plane, Integer startTime, Integer duration, Integer price) {
+        this.plane = plane;
+        this.startTime = startTime;
+        this.endTime = this.startTime + duration;
+        this.price = price;
+        this.duration = duration;
+    }
+
+
+}
