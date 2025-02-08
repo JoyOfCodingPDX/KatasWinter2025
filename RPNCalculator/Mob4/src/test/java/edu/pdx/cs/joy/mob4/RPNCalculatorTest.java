b979162766aabea9aebeac0f4d5d1684e18e1893
David Whitlock <david.m.whitlock@gmail.com>
2025-02-07 21:23:06 -0800

    Mob 4

---
+++ RPNCalculator/Mob4/src/test/java/edu/pdx/cs/joy/mob4/RPNCalculatorTest.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs.joy.mob4;
+
+import org.junit.jupiter.api.Test;
+
+public class RPNCalculatorTest
+{
+
+  @Test
+  void canInstantiateKataClass() {
+    new RPNCalculator();
+  }
+
+}
