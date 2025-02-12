41c18ce0c99dfae289e92760bf9989e83db9f4fc
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-02-11 22:58:46 -0800

    Made More Progress on Lags Kata

--- Lags/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Lags.java
+++ Lags/Pair4/src/main/java/edu/pdx/cs/joy/pair4/Lags.java
@@ -1,6 +1,8 @@
 package edu.pdx.cs.joy.pair4;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -31,14 +33,29 @@ public class Lags {
       return;
     }
     List<Flight> flights = parse(args);
-    solve(flights);
+    List<List<Flight>> combinations = getCandidateFlights(flights);
+    Optional<List<Flight>> result = combinations.stream().sorted((flightList1, flightList2) -> Integer.valueOf(flightList2.stream().mapToInt(flight -> flight.money).reduce(0, (f1, f2) -> f1 + f2)).compareTo(flightList1.stream().mapToInt(flight -> flight.money).reduce(0, (f1, f2) -> f1 + f2))).findFirst();
+    if (result.isEmpty()) {
+      System.out.println("No values provided");
+    } else {
+      System.out.println("The best set of flights to take is " + getFlightSequence(result.get()));
+    }
   }
 
-  public static List<Flight> solve(List<Flight> flights) {
+  public static List<List<Flight>> getCandidateFlights(List<Flight> flights) {
     Flight flight = flights.get(flights.size() - 1);
-    List<Flight> candidateFlights = flights.stream().filter((Flight currentFlight) -> flight.endTime <= currentFlight.startTime).toList();
-//    candidateFlights.stream().map((candidateFlight) -> new List) // Combination of flights
-//    List<List<Flight>> = //
-    return List.of();
+    List<List<Flight>> candidateSequences = flights
+    .stream()
+    .filter((Flight currentFlight) -> flight.endTime <= currentFlight.startTime)
+    .map((candidateFlight) -> {
+      List<Flight> result = new ArrayList<>(flights);
+      result.add(candidateFlight);
+      return result;
+    }).toList();
+    return candidateSequences;
+  }
+
+  public static String getFlightSequence(List<Flight> flights) {
+    return String.join(" -> ", flights.stream().map((f) -> f.id).toList());
   }
 }
