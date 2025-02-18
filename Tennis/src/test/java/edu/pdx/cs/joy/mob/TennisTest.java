814f14ab82b545d4e1f003aee6fa8cb03bf61a23
GoodGuyGregory <greg.witt625@gmail.com>
2025-02-17 19:42:12 -0800

    adds beautiful refactor

--- Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
+++ Tennis/src/test/java/edu/pdx/cs/joy/mob/TennisTest.java
@@ -31,39 +31,36 @@ public class TennisTest
   @Test
   void bothPlayersAtDeuce() {
     Tennis tennis = new Tennis();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerTwoScores();
-    tennis.playerTwoScores();
-    tennis.playerTwoScores();
+    playerOneScores(tennis, 3);
+    playerTwoScores(tennis, 3);
     assertThat(tennis.getScore(), equalTo("Deuce"));
   }
 
   @Test
   void playerOneHadAdvantage() {
     Tennis tennis = new Tennis();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerTwoScores();
-    tennis.playerTwoScores();
-    tennis.playerTwoScores();
+    playerOneScores(tennis, 4);
+    playerTwoScores(tennis, 3);
     assertThat(tennis.getScore(), equalTo("Advantage: Player One"));
 }
+
+  private static void playerOneScores(Tennis tennis, int scores) {
+    for (int i = 0; i < scores; i++) {
+      tennis.playerOneScores();
+    }
+  }
+
+  private static void playerTwoScores(Tennis tennis, int scores) {
+    for (int i = 0; i < scores; i++) {
+      tennis.playerTwoScores();
+    }
+  }
 
   @Test
   void bothPlayersAtDeuceFourFour() {
     Tennis tennis = new Tennis();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerOneScores();
-    tennis.playerTwoScores();
-    tennis.playerTwoScores();
-    tennis.playerTwoScores();
-    tennis.playerTwoScores();
+    playerOneScores(tennis, 4);
+    playerTwoScores(tennis, 4);
     assertThat(tennis.getScore(), equalTo("Deuce"));
 
   }
