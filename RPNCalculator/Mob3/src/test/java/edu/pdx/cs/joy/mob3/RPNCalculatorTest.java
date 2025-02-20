f68447c5506820afb4fadd2627b6ac5026de1ebd
TejaswiBattula <b.venkatteja@gmail.com>
2025-02-19 19:21:58 -0800

    added tests for cal

--- RPNCalculator/Mob3/src/test/java/edu/pdx/cs/joy/mob3/RPNCalculatorTest.java
+++ RPNCalculator/Mob3/src/test/java/edu/pdx/cs/joy/mob3/RPNCalculatorTest.java
@@ -1,5 +1,8 @@
 package edu.pdx.cs.joy.mob3;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class RPNCalculatorTest
@@ -10,4 +13,24 @@ public class RPNCalculatorTest
     new RPNCalculator();
   }
 
+  @Test
+  void testForEmptyString(){
+    RPNCalculator rcal = new RPNCalculator();
+    int value = rcal.parseOperators("");
+    assertEquals(value,0);
+  }
+
+  @Test
+  void testForASingleString(){
+    RPNCalculator rcal = new RPNCalculator();
+    int value = rcal.parseOperators("1");
+    assertEquals(value,1);
+  }
+
+  @Test
+  void testForTwoStrings(){
+    RPNCalculator rcal = new RPNCalculator();
+    int value = rcal.parseOperators("1 2");
+    assertThrows(IllegalArgumentException.class, "bad argument");
+  }
 }
