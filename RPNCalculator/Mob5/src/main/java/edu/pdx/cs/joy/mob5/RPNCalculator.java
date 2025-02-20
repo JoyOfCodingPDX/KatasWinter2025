214829d38f67671728732804abfa3102a27b1957
jonah pattison <JPatts@Hp-2.local>
2025-02-19 19:47:21 -0800

    jonah made changes

--- RPNCalculator/Mob5/src/main/java/edu/pdx/cs/joy/mob5/RPNCalculator.java
+++ RPNCalculator/Mob5/src/main/java/edu/pdx/cs/joy/mob5/RPNCalculator.java
@@ -1,7 +1,10 @@
 package edu.pdx.cs.joy.mob5;
 
 import com.google.common.annotations.VisibleForTesting;
+
 import java.util.*;
+
+import org.checkerframework.checker.units.qual.Length;
 
 /**
  * A class for getting started with a code kata
@@ -15,11 +18,27 @@ public class RPNCalculator {
   public static void main(String[] args) {
     Stack<Integer> stack = new Stack();
     System.out.println("Answer: " + calculate(args));
+
+    // for loop in the iterates from left to right
+    for (int i = 0; i < args.length; i++) {
+        // each integer encountered add to stack
+        // check to see if integer
+        if (args[i].matches("[0-9]*"))
+        {
+            // push i onto stack
+            stack.push(Integer.parseInt(args[i]));
+        }
+        else if {} // if operand
+        else {} // errors 
+        // each operand encountered preform calculate function
+
+    })
   }
 
-  private static int calculate(String[] args)
+  private static int calculate(Integer a, Integer b, String operand)
   {
-    
+    // result = a operand b 
+
     return 0;
   }
 
