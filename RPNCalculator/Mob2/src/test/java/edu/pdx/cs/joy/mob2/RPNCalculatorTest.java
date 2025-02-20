d1f6003269f2c054987fc9c00fd3ea21e023a1a9
cristianionion <ion.cristian123@gmail.com>
2025-02-19 19:47:06 -0800

    akira driver

--- RPNCalculator/Mob2/src/test/java/edu/pdx/cs/joy/mob2/RPNCalculatorTest.java
+++ RPNCalculator/Mob2/src/test/java/edu/pdx/cs/joy/mob2/RPNCalculatorTest.java
@@ -2,32 +2,32 @@ package edu.pdx.cs.joy.mob2;
 
 import org.junit.jupiter.api.Test;
 import java.util.Stack;
-import static org.hamcrest.*;
 import edu.pdx.cs.joy.InvokeMainTestCase;
-import edu.pdx.cs.joy.ParserException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 
 public class RPNCalculatorTest
 {
-
+  
   private MainMethodResult invokeMain(String...args){
     return invokeMain(RPNCalculator.class,args);
   }
-
+  
 
   @Test
   void canInstantiateKataClass() {
     new RPNCalculator();
   }
-
+  /* 
   @Test
   void baseCase(){
     MainMethodResult result = invokeMain("3","5","+");
     assertThat(result.getTextWrittenToStandardOut(),containsString("8"));
   }
+    */
 
 }
