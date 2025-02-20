a51145a3b7021b8d4344d4bcc512046b736c6701
nmetens <metens@pdx.edu>
2025-02-19 19:56:41 -0800

    Switch statement

--- RPNCalculator/Mob5/src/main/java/edu/pdx/cs/joy/mob5/RPNCalculator.java
+++ RPNCalculator/Mob5/src/main/java/edu/pdx/cs/joy/mob5/RPNCalculator.java
@@ -14,10 +14,19 @@ import org.checkerframework.checker.units.qual.Length;
  */
 public class RPNCalculator {
 
+    
   @VisibleForTesting
   public static void main(String[] args) {
     Stack<Integer> stack = new Stack();
     System.out.println("Answer: " + calculate(args));
+
+    Stack<Integer> a_stack = new Stack<>();
+    List<String> operands = new ArrayList<String>();
+    operands.add("+");
+    operands.add("-");
+    operands.add("*");
+    operands.add("/"); 
+    operands.add("MAX");
 
     // for loop in the iterates from left to right
     for (int i = 0; i < args.length; i++) {
@@ -27,18 +36,35 @@ public class RPNCalculator {
         {
             // push i onto stack
             stack.push(Integer.parseInt(args[i]));
-        }
-        else if {} // if operand
-        else {} // errors 
+        } else {
+            calculate(stack.pop(), stack.pop(), args[i]);
+            //System.err.println("ERROR"); 
+            //throw new Exception("ERROR");
+        } // errors 
         // each operand encountered preform calculate function
 
-    })
+    }
   }
 
   private static int calculate(Integer a, Integer b, String operand)
   {
-    // result = a operand b 
-
+    switch (operand) {
+        case "+":
+            return a + b;
+            break;
+        case "-":
+            return a - b;
+            break;
+        case "+":
+            break;
+        case "/":
+            return a / b;
+            break;
+        case "MAX":
+            return max(a, b); 
+            break;
+        default:
+    }
     return 0;
   }
 
