f62802fe6c93b920fed6ca921f9d60b30c6e9a6e
iit2018062 <iit2018062@iiita.ac.in>
2025-02-19 19:32:51 -0800

    test cases corrected

--- RPNCalculator/Mob3/src/test/java/edu/pdx/cs/joy/mob3/RPNCalculatorTest.java
+++ RPNCalculator/Mob3/src/test/java/edu/pdx/cs/joy/mob3/RPNCalculatorTest.java
@@ -1,7 +1,11 @@
 package edu.pdx.cs.joy.mob3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RPNCalculatorTest
 {
@@ -9,25 +13,6 @@ public class RPNCalculatorTest
   @Test
   void canInstantiateKataClass() {
     new RPNCalculator();
-  }
-
-  @Test
-  void testAddition() {
-    assertEquals(5, RPNCalculator.parseOperators("2 3 +"));
-  }
-  @Test
-  void testSubtraction(){
-    assertEquals(-1, RPNCalculator.parseOperators("2 3 -"));
-  }
-
-  @Test
-  void testMultiplication(){
-    assertEquals(6, RPNCalculator.parseOperators("2 3 *"));
-  }
-
-  @Test
-  void testDivision(){
-    assertEquals(2, RPNCalculator.parseOperators("6 3 /"));
   }
 
   @Test
@@ -44,11 +29,17 @@ public class RPNCalculatorTest
     assertEquals(value,1);
   }
 
-  // @Test
-  // void testForTwoStrings(){
-  //   RPNCalculator rcal = new RPNCalculator();
-  //   int value = rcal.parseOperators("1 2");
-  //   assertThrows(IllegalArgumentException.class, "bad argument");
-  // }
+  @Test
+  void testForTwoStrings() {
+    RPNCalculator rcal = new RPNCalculator();
+    IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> {
+      rcal.parseOperators("1 2");
+    });
 
+    assertEquals("bad argument", thrown.getMessage());
+  }
+  @Test
+  void testAddition() {
+    assertEquals(5, RPNCalculator.parseOperators("2 3 +"));
+  }
 }
