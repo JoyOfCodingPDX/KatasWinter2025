e67b745f7d5ff7911053862f2004bbb5bc8770f1
satvikmudgal <satvik.mudgal@gmail.com>
2025-02-26 19:50:22 -0800

    Satvik Driving Commit

--- BankOCR/Mob2/src/main/java/edu/pdx/cs/joy/mob2/BankOCR.java
+++ BankOCR/Mob2/src/main/java/edu/pdx/cs/joy/mob2/BankOCR.java
@@ -2,6 +2,8 @@ package edu.pdx.cs.joy.mob2;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.ArrayList;
+
 /**
  * A class for getting started with a code kata
  *
@@ -12,6 +14,27 @@ public class BankOCR {
 
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+
+    String line1 = args[0];
+    String line2 = args[1];
+    String line3 = args[2];
+    String argString = line1 + "\n" + line2 + "\n" + line3;
+
+    ArrayList<String> numFormats = new ArrayList<>();
+    numFormats.add(" _ \n| |\n|_|");
+    numFormats.add("  \n  |\n  |");
+    numFormats.add(" _\n _|\n _|");
+    numFormats.add("  \n|_|\n  |");
+    numFormats.add(" _\n|_\n _|");
+    numFormats.add(" _\n|_\n|_|");
+    numFormats.add(" _\n  |\n  |");
+    numFormats.add(" _\n|_|\n|_|");
+    numFormats.add(" _\n|_|\n _|");
+
+    System.out.println(numFormats.get(0));
+    if (argString.equals(numFormats.get(0))) {
+      System.out.println("0");
+    }
+
   }
 }
