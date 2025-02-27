eeabc0aa268f89726b44745f18dc40561a0b80cc
Ahmed-Al-Badri <albadri@pdx.edu>
2025-02-26 19:30:47 -0800

    More work needed.

--- BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
+++ BankOCR/Mob3/src/main/java/edu/pdx/cs/joy/mob3/BankOCR.java
@@ -14,4 +14,23 @@ public class BankOCR {
   public static void main(String[] args) {
     System.err.println("Missing command line arguments");
   }
+
+  public static String parseArgs(char[][] args) {
+    if (args.length < 3){
+      throw new Exception("The amount of lines is less than three");
+    }
+
+    for (int i = 0; i < 3; i++){
+      if (args[i].length != 27){
+        throw new Exception("The lines are not 27 chars longs");
+      }
+    }
+
+    // 1 0 0...
+    // 0 0 ... 1
+    // 1 0 0 0 0 0 0 0 0
+    // 0 0 0 0 0 0 0 0 1
+
+    return "";
+  }
 }
