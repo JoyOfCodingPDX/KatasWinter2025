1f54c7c2b4678682ba6a437f48c2fda8792a6e78
GoodGuyGregory <greg.witt625@gmail.com>
2025-02-26 20:15:33 -0800

    end of Mobbing

--- BankOCR/Mob1/src/main/java/edu/pdx/cs/joy/mob1/BankOCR.java
+++ BankOCR/Mob1/src/main/java/edu/pdx/cs/joy/mob1/BankOCR.java
@@ -2,6 +2,10 @@ package edu.pdx.cs.joy.mob1;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * A class for getting started with a code kata
  *
@@ -9,13 +13,56 @@ import com.google.common.annotations.VisibleForTesting;
  * class (and its tests).
  */
 public class BankOCR {
+  private static final Map<String, String> bankOCRMap = new HashMap<>();
+
+  String zero = " _ | ||_|";
+  String one  = "     |  |";
+  String two  = " _  _||_ ";
+  String three= " _  _| _|";
+  String four = "   |_|  |";
+  String five = " _ |_   |";
+  String six  = " _ |_ |_|";
+  String seven= " _   |  |";
+  String eight= " _ |_||_|";
+  String nine = " _ |_| _|";
+
+  public BankOCR(){
+    bankOCRMap.put(zero, "0");
+    bankOCRMap.put(one, "1");
+    bankOCRMap.put(two, "2");
+    bankOCRMap.put(three, "3");
+    bankOCRMap.put(four, "4");
+    bankOCRMap.put(five, "5");
+    bankOCRMap.put(six, "6");
+    bankOCRMap.put(seven, "7");
+    bankOCRMap.put(eight, "8");
+    bankOCRMap.put(nine, "9");
+  }
+
 
   @VisibleForTesting
   public static void main(String[] args) {
-    System.err.println("Missing command line arguments");
+
   }
 
-  public String parseOCR(String zero) {
-    return "bonk";
+  public static String parseOCR(String num) {
+
+
+    return bankOCRMap.getOrDefault(num, "?");
   }
+
+  public static String somethingCool(String top, String mid, String bot) {
+    String[] input = new String[]{"", "", "", "", "", "", "", "", ""};
+    String output = "";
+
+    for (int j = 0; j < 27; j+=3) {
+
+      output = output.concat(parseOCR(top.substring(j, j+3) +
+              mid.substring(j, j+3) + bot.substring(j, j+3)));
+    }
+    return output;
+  }
+
+
+
 }
--- BankOCR/Mob1/src/test/java/edu/pdx/cs/joy/mob1/BankOCRTest.java
+++ BankOCR/Mob1/src/test/java/edu/pdx/cs/joy/mob1/BankOCRTest.java
@@ -18,17 +18,37 @@ public class BankOCRTest
     // ONE DIGIT:
     @Test
     void canTestOneSingleZero() {
-      BankOCR bankOCR = new BankOCR();
-    /*
-       _
-      | |
-      |_|
-       */
 
       String zero = " _ | ||_|";
 
-      assertThat(bankOCR.parseOCR(zero), equalTo("0"));
+      assertThat(BankOCR.parseOCR(zero), equalTo("0"));
     }
 
+@Test
+    void canTestOneSingleONE(){
 
+
+      String one = "     |  |";
+
+      assertThat(BankOCR.parseOCR(one), equalTo("1"));
+    }
+@Test
+  void canTestOneSingleTWO(){
+
+    String two = " _  _||_ ";
+    assertThat(BankOCR.parseOCR(two), equalTo("2"));
+}
+
+@Test
+  void canTestNineNumbers() {
+
+    String topLine = "    _  _     _  _  _  _  _ ";
+    String midLine = "  | _| _||_||_ |_   ||_||_|";
+    String botLine = "  ||_  _|  | _||_|  ||_| _|";
+
+    String returned = BankOCR.somethingCool(topLine, midLine, botLine);
+
+    assertThat(returned, equalTo("123456789"));
+
+  }
 }
