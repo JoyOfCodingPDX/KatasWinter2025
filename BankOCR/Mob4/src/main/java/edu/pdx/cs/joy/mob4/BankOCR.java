daac62f21f198d829c0e3aebadde849641fd7975
Max <maxwrego@gmail.com>
2025-02-26 20:21:29 -0800

    Group 4

--- BankOCR/Mob4/src/main/java/edu/pdx/cs/joy/mob4/BankOCR.java
+++ BankOCR/Mob4/src/main/java/edu/pdx/cs/joy/mob4/BankOCR.java
@@ -3,9 +3,7 @@ package edu.pdx.cs.joy.mob4;
 import com.google.common.annotations.VisibleForTesting;
 
 import java.sql.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * A class for getting started with a code kata
@@ -14,6 +12,8 @@ import java.util.List;
  * class (and its tests).
  */
 public class BankOCR {
+
+
 
   @VisibleForTesting
   public static void main(String[] args) {
@@ -32,17 +32,29 @@ public class BankOCR {
   }
 
   public int parseNumber(String testNumber) {
-    System.err.println("Missing command line arguments");
+    //System.err.println("Missing command line arguments");
+
+    Map<Numbers, Integer> map = new HashMap<>();
+    Numbers four = new Numbers("   ", "|_|","  |");
+    map.put(four, 4);
 
     //turn into array stream
     ArrayList<String> lines = new ArrayList<>(Arrays.asList(testNumber.split("\n")));
     List<ArrayList<String>> stringList = lines.stream().map(((line) -> splitByLength(line))).toList();
 
+    ArrayList<Numbers> numbers = new ArrayList<>();
+    while(!stringList.get(0).isEmpty()){
+      Numbers num = new Numbers(stringList.get(0).get(0),stringList.get(1).get(0),stringList.get(2).get(0) );
+      numbers.add(num);
+      stringList.get(0).remove(0);
+      stringList.get(1).remove(0);
+      stringList.get(2).remove(0);
 
+    }
 
-    return 0;
+    return map.get(four);
   }
 
-  Numbers four = new Numbers("   ", "|_|","  |");
+
 
 }
