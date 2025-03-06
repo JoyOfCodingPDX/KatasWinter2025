703a01d9de57d5be8dcfa7ce2203ad56a564b073
nmetens <metens@pdx.edu>
2025-03-05 19:37:03 -0800

    Nathan, started tests for mine

--- Minesweeper/Mob5/src/test/java/edu/pdx/cs/joy/mob5/MinesweeperTest.java
+++ Minesweeper/Mob5/src/test/java/edu/pdx/cs/joy/mob5/MinesweeperTest.java
@@ -9,5 +9,25 @@ public class MinesweeperTest
   void canInstantiateKataClass() {
     new Minesweeper();
   }
+  
 
+  @Test
+  void fourByFour() {
+    List<List<String>> mineField = List.of(
+            List.of("*", ".", ".", "."),
+            List.of(".", ".", ".", "."),
+            List.of(".", "*", ".", "."),
+            List.of(".", ".", ".", "."),
+            List.of(".", ".", ".", ".")
+    );
+
+    List<List<String>> mineFieldResult = List.of(
+           List.of("*", "1", "0", "0"),
+           List.of("2", "2", "1", "0"),
+           List.of("1", "*", "1", "0"),
+           List.of("1", "1", "1", "0"),
+    );
+    
+    assertThat(Minesweeper.MineSweepSolve(mineField), mineFieldResult);
+  }
 }
