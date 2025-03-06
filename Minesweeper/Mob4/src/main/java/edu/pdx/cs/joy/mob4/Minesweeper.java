80541008f212e3f77c910f40f1a31833c3dc41a4
TejaswiBattula <b.venkatteja@gmail.com>
2025-03-05 19:42:23 -0800

    pushed main changes

--- Minesweeper/Mob4/src/main/java/edu/pdx/cs/joy/mob4/Minesweeper.java
+++ Minesweeper/Mob4/src/main/java/edu/pdx/cs/joy/mob4/Minesweeper.java
@@ -1,5 +1,7 @@
 package edu.pdx.cs.joy.mob4;
 
+import java.text.CharacterIterator;
+
 import com.google.common.annotations.VisibleForTesting;
 
 /**
@@ -10,30 +12,50 @@ import com.google.common.annotations.VisibleForTesting;
  */
 public class Minesweeper {
 
-public static char[][] solve(char[][] grid) {
 
-  int rows = grid.length;
-  int cols = grid[0].length;
+  public boolean valid(int i, int j, int n, int m){
+    return i>=0 && j>=0 && i<n && j<m;
+  }
+
+public char[][] solve(int rows, int cols, char[][] grid) {
+
   char[][] result = new char[rows][cols];
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
-      result[i][j] = grid[i][j];
+      // result[i][j] = grid[i][j];
       if (grid[i][j] == '*') {
+
+        if(valid(i-1,j-1,rows, cols) && grid[i-1][j-1]!='*'){
+          int val = Character.getNumericValue(grid[i-1][j-1]);
+          val++;
+          grid[i-1][j-1] = (char) (val + '0'); //'1'+'0'
+        }
+        if(valid(i-1,j-1,rows, cols) && grid[i-1][j-1]!='*'){
+          int val = Character.getNumericValue(grid[i-1][j-1]);
+          val++;
+          grid[i-1][j-1] = (char) (val+'0');
+        }
+
+
 
       }
     }
   }
     return result;
 }
+
+
   @VisibleForTesting
   public static void main(String[] args)
   {
     char[][] inputGrid={
-            {'*','.','.'},
+            {'*','0','0'},
             {'*','.','.'}
 
     };
-
+    
+  int rows = grid.length;
+  int cols = grid[0].length;
     System.err.println("Missing command line arguments");
   }
 }
