c4d9c36f738a79bd97273ac24d1df817661c663e
Dylan Conklin <74876741+dylanconklin@users.noreply.github.com>
2025-03-05 19:19:32 -0800

    .

--- Minesweeper/Mob5/src/main/java/edu/pdx/cs/joy/mob5/Minesweeper.java
+++ Minesweeper/Mob5/src/main/java/edu/pdx/cs/joy/mob5/Minesweeper.java
@@ -2,6 +2,7 @@ package edu.pdx.cs.joy.mob5;
 
 import com.google.common.annotations.VisibleForTesting;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,16 +26,24 @@ public class Minesweeper {
     int xLimit = matrix.get(0).size();
     int yLimit = matrix.size();
 
-    List<List<String>> matrix2 = List.copyOf(matrix);
-//    List<List<int>> matrix2 = List.copyOf(matrix);
-    for(int i = 0; i < matrix2.size(); i++) {
-      for(int j = 0; j < matrix2.get(i).size(); j++) {
-
+    ArrayList<ArrayList<Pair>> coordinateMatrix = new ArrayList<>();
+    for(int i = 0; i < matrix.size(); i++) {
+      ArrayList<Pair> row = new ArrayList<Pair>();
+      for(int j = 0; j < matrix.get(i).size(); j++) {
+        row.add(new Pair(i, j));
       }
+      coordinateMatrix.add(row);
     }
+
+    List<List<Integer>> result = coordinateMatrix.stream().map(row -> row.stream().map(e -> numOfCoordsNearby()));
+  }
+
+  int numOfCoordsNearby() {
+    return 0;
   }
 
   public static int clamp(int value, int limit) {
     return (value < 0) ? 0 : Math.min(value, limit);
   }
 }
+
