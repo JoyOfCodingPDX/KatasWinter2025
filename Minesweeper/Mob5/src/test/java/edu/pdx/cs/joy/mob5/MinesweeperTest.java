2509612ff9a581793cdbc9ed068d5026320224d0
nmetens <metens@pdx.edu>
2025-03-05 19:44:23 -0800

    More tests for 3x3 matrix

--- Minesweeper/Mob5/src/test/java/edu/pdx/cs/joy/mob5/MinesweeperTest.java
+++ Minesweeper/Mob5/src/test/java/edu/pdx/cs/joy/mob5/MinesweeperTest.java
@@ -30,4 +30,22 @@ public class MinesweeperTest
     
     assertThat(Minesweeper.MineSweepSolve(mineField), mineFieldResult);
   }
+
+  @Test
+  void threeBythree() {
+    List<List<String>> mineField = List.of(
+            List.of("*", "*", ".", ".", "."),
+            List.of(".", ".", ".", ".", "."),
+            List.of(".", ".", ".", ".", "."),
+            List.of(".", "*", ".", ".", "."),
+    );
+    List<List<String>> mineField = List.of(
+            List.of("*", "*", "1", "0", "0"),
+            List.of("3", "3", "2", "0", "0"),
+            List.of("1", "*", "1", "0", "0"),
+            );
+
+    assertThat(Minesweeper.MineSweepSolve(mineField), mineFieldResult);
+  }
+  }
 }
