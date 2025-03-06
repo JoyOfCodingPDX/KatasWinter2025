17eef5f9527a25ebafb2c4d2010d82d375429bdf
GoodGuyGregory <greg.witt625@gmail.com>
2025-03-05 19:19:52 -0800

    adds initial grid builder

--- Minesweeper/Mob1/src/test/java/edu/pdx/cs/joy/mob1/MinesweeperTest.java
+++ Minesweeper/Mob1/src/test/java/edu/pdx/cs/joy/mob1/MinesweeperTest.java
@@ -2,12 +2,49 @@ package edu.pdx.cs.joy.mob1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class MinesweeperTest
 {
 
   @Test
   void canInstantiateKataClass() {
+
     new Minesweeper();
+
+
+    // inital test ideas:
+//    3 x 3
+    @Test
+    void createGridFromInputNums() {
+      Minesweeper minesweeper = new Minesweeper();
+      minesweeper.createGrid( 3,  3);
+
+      char[][] grid = minesweeper.getGrid();
+      assertThat(grid.getWidth(), 3);
+      assertThat(grid.getHeight(), 3);
+
+    }
+
+    // 4 x 4
+
+    // 0 x 0
+
+    // randomized mine dropper?
+
+    // DEAD MIDDLE MINE TEST:
+    // 3 x 3
+
+    // . . .
+    // . * .
+    // . . .
+
+    // assumption that results in
+
+    // 1 1 1
+    // 1 * 1
+    // 1 1 1
+
   }
 
 }
